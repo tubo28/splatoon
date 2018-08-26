e651c53d7b42e4218bc92b95c612677dd8dc4d4a
tubo28 <arsenic28@gmail.com>
2018-08-26 18:27:30 +0900

    refactor

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,78 +5,131 @@ using System.Diagnostics;
 
 namespace HelloWorld
 {
-    class Template
-    {
-        Mat binMat;
-        readonly byte[] temporaryArray;
-        readonly byte[] array;
-        readonly int size;
-
-        Rect rect;
-
-        public Template(Mat mat, Rect rect)
-        {
-            binMat = Mat.Zeros(new Size(mat.Width, mat.Height), MatType.CV_8SC1);
-            for (int i = 0; i < mat.Height; i++)
-            {
-                for (int j = 0; j < mat.Width; j++)
-                {
-                    var colorVec = mat.Get<Vec3b>(i, j);
-                    var c = (byte)(Math.Max(colorVec[0], Math.Max(colorVec[1], colorVec[2])) == 0 ? 0 : 255);
-                    binMat.Set(i, j, new Vec3b(c, c, c));
-                }
-            }
-
-            size = binMat.Height * binMat.Width;
-
-            array = new byte[size];
-            binMat.GetArray(0, 0, array);
-
-            temporaryArray = new byte[size];
-
-            this.rect = rect;
-        }
-
-        public bool Match(Mat binMat)
-        {
-            return Likelihood(binMat) >= 0.7;
-        }
-
-        double Likelihood(Mat binMat)
-        {
-            var cropped = new Mat(binMat, rect);
-            cropped.GetArray(0, 0, temporaryArray);
-
-            var den = 0;
-            var nume = 0;
-            for (int i = 0; i < size; i++)
-            {
-                if (array[i] == 0)
-                {
-                    den++;
-                    if (temporaryArray[i] == 0)
-                    {
-                        nume++;
-                    }
-                }
-            }
-            return 1.0 * nume / den;
-        }
-    }
-
     class Program
     {
+        class Template
+        {
+            Mat binMat;
+            readonly byte[] temporaryArray;
+            readonly byte[] array;
+            readonly int size;
 
-        static Template MatchStartTemplate = null;
-        static Template MatchEndTemlate = null;
-        static long babyStep = 500;
-        static long giantStep = 2000;
+            Rect rect;
+
+            public Template(Mat mat, Rect rect)
+            {
+                binMat = Mat.Zeros(new Size(mat.Width, mat.Height), MatType.CV_8SC1);
+                for (int i = 0; i < mat.Height; i++)
+                {
+                    for (int j = 0; j < mat.Width; j++)
+                    {
+                        var colorVec = mat.Get<Vec3b>(i, j);
+                        var c = (byte)(Math.Max(colorVec[0], Math.Max(colorVec[1], colorVec[2])) == 0 ? 0 : 255);
+                        binMat.Set(i, j, new Vec3b(c, c, c));
+                    }
+                }
+
+                size = binMat.Height * binMat.Width;
+
+                array = new byte[size];
+                binMat.GetArray(0, 0, array);
+
+                temporaryArray = new byte[size];
+
+                this.rect = rect;
+            }
+
+            public bool Match(Mat binMat)
+            {
+                return Likelihood(binMat) >= 0.7;
+            }
+
+            double Likelihood(Mat binMat)
+            {
+                var cropped = new Mat(binMat, rect);
+                cropped.GetArray(0, 0, temporaryArray);
+
+                var den = 0;
+                var nume = 0;
+                for (int i = 0; i < size; i++)
+                {
+                    if (array[i] == 0)
+                    {
+                        den++;
+                        if (temporaryArray[i] == 0)
+                        {
+                            nume++;
+                        }
+                    }
+                }
+                return 1.0 * nume / den;
+            }
+
+            public Tuple<long, long> FindMatchBoundary(VideoCapture video, long framePosMsec)
+            {
+                return Tuple.Create(FindMatchStart(video, framePosMsec), FindMatchEnd(video, framePosMsec));
+            }
+
+            static Mat searchFrame = new Mat();
+            static Mat searchBinFrame = new Mat();
+
+            private long FindMatchStart(VideoCapture video, long fromPosMsec)
+            {
+                var m = fromPosMsec;
+                while (true)
+                {
+                    var tryMsec = m - babyStep;
+                    if (tryMsec < 0)
+                    {
+                        break;
+                    }
+
+                    GetFrameByMsec(video, tryMsec, searchFrame);
+                    Binarize(searchFrame, 7, searchBinFrame);
+                    if (!Match(searchBinFrame))
+                    {
+                        break;
+                    }
+                    Console.WriteLine($"match starts at {video.Get(CaptureProperty.PosFrames)} frm");
+                    m = tryMsec;
+                }
+                return m;
+            }
+
+            private long FindMatchEnd(VideoCapture video, long fromPosMsec)
+            {
+                var m = fromPosMsec;
+                while (true)
+                {
+                    var tryMsec = m + babyStep;
+                    if (tryMsec >= video.Fps * video.FrameCount)
+                    {
+                        break;
+                    }
+
+                    GetFrameByMsec(video, tryMsec, searchFrame);
+                    Binarize(searchFrame, 7, searchBinFrame);
+                    if (!Match(searchBinFrame))
+                    {
+                        break;
+                    }
+                    Console.WriteLine($"match ends at   {video.Get(CaptureProperty.PosFrames)} frm");
+                    m = tryMsec;
+                }
+                return m;
+            }
+        }
+
+        static Template GameStartTemplate = null;
+        static Template GameEndTemlate = null;
+
+        static long babyStep = 200;
+        static long giantStep = 2500;
 
         static void Main(string[] args)
         {
-            MatchStartTemplate = new Template(Cv2.ImRead("waku.png"), new Rect(688, 289, 541, 250));
-            MatchEndTemlate = new Template(Cv2.ImRead("p.png"), new Rect(237, 68, 50, 83));
-            
+            GameStartTemplate = new Template(Cv2.ImRead("waku.png"), new Rect(688, 289, 541, 250));
+            GameEndTemlate = new Template(Cv2.ImRead("p.png"), new Rect(237, 68, 50, 83));
 
             if (false)
             {
@@ -117,9 +170,7 @@ namespace HelloWorld
 
                 var srcFrame = new Mat();
                 var binFrame = new Mat();
-                var searchFrame = new Mat();
-                var searchBinFrame = new Mat();
-
+                
                 var posMsec = 0L;
                 while (true)
                 {
@@ -133,76 +184,27 @@ namespace HelloWorld
                     }
 
                     Binarize(srcFrame, 7, binFrame);
-                    if (MatchStartTemplate.Match(binFrame))
+                    if (GameStartTemplate.Match(binFrame))
                     {
-                        Console.WriteLine($"match starts at {video.Get(CaptureProperty.PosFrames)} frm");
-                        var t = FindMatchStart(video, searchFrame, searchBinFrame, posMsec);
+                        var se = GameStartTemplate.FindMatchBoundary(video, posMsec);
+                        var start = se.Item1;
+                        var end = se.Item2;
+                        Console.WriteLine($"{start:0000000}ms: match starts");
+                        nextPosMsec = end + babyStep;
                     }
-                    else if(MatchEndTemlate.Match(binFrame))
+                    else if (GameEndTemlate.Match(binFrame))
                     {
-                        Console.WriteLine($"match ends at   {video.Get(CaptureProperty.PosFrames)} frm");
-                        nextPosMsec = FindMatchEnd(video, searchFrame, searchBinFrame, posMsec);
+                        var se = GameEndTemlate.FindMatchBoundary(video, posMsec);
+                        var end = se.Item2;
+                        Console.WriteLine($"{end:0000000}ms: match ends");
+                        nextPosMsec = end + babyStep;
                     }
                     posMsec = nextPosMsec;
                 }
             }
         }
 
-        private static long FindMatchEnd(VideoCapture video, Mat searchFrame, Mat searchBinFrame, long fromPosMsec)
-        {
-            long res = 0;
-            var m = fromPosMsec;
-            while (true)
-            {
-                var tryMsec = m + babyStep;
-                if (tryMsec >= video.Fps * video.FrameCount)
-                {
-                    break;
-                }
-
-                res = tryMsec + giantStep;
-                GetFrameByMsec(video, tryMsec, searchFrame);
-                Binarize(searchFrame, 7, searchBinFrame);
-                if (!MatchEndTemlate.Match(searchBinFrame))
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine($"match ends at   {video.Get(CaptureProperty.PosFrames)} frm");
-                }
-                m = tryMsec;
-            }
-            return res;
-        }
-
-        private static long FindMatchStart(VideoCapture video, Mat searchFrame, Mat searchBinFrame, long fromPosMsec)
-        {
-            var res = fromPosMsec;
-            var m = fromPosMsec;
-            while (true)
-            {
-                var tryMsec = m - babyStep;
-                if (tryMsec < 0)
-                {
-                    break;
-                }
-
-                GetFrameByMsec(video, tryMsec, searchFrame);
-                Binarize(searchFrame, 7, searchBinFrame);
-                if (!MatchStartTemplate.Match(searchBinFrame))
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine($"match starts at {video.Get(CaptureProperty.PosFrames)} frm");
-                    res = tryMsec;
-                }
-                m = tryMsec;
-            }
-            return res;
-        }
+       
 
         static void GetFrameByMsec(VideoCapture video, long ms, Mat res)
         {
